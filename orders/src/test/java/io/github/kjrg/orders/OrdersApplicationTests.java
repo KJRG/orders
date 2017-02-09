1f9e657a6d528a8d705c66ba42af0ba2f5710f26
KJRG <kjrgalka@gmail.com>
2017-02-09 23:13:39 +0100

    Add initial project and update .gitignore

---
+++ orders/src/test/java/io/github/kjrg/orders/OrdersApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.kjrg.orders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
