81696953aa080073e988a51cb18b130f5cf88b1e
KJRG <kjrgalka@gmail.com>
2017-02-21 00:06:23 +0100

    Configure logging with Logback and add sample logging

--- orders/src/main/java/io/github/kjrg/orders/service/impl/CountryServiceImpl.java
+++ orders/src/main/java/io/github/kjrg/orders/service/impl/CountryServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,11 +23,14 @@ import io.github.kjrg.orders.service.CountryService;
 @Service
 public class CountryServiceImpl implements CountryService {
 
+	private final Logger LOG = LoggerFactory.getLogger(CountryServiceImpl.class);
+	
 	@Autowired
 	private CountryRepository countryRepository;
 
 	@Override
 	public List<Country> findAllCountries() {
+		LOG.info("Finding all countries in the database");
 		return StreamSupport.stream(countryRepository.findAll().spliterator(), false).collect(Collectors.toList());
 	}
 }
